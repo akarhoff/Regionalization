63a63af74e107d83db0b56a1e9ec51c449fc92a4
akarhoff <34249314+akarhoff@users.noreply.github.com>
2019-06-05 17:09:13 -0400

    Add files via upload

---
+++ regionalization.js
@@ -0,0 +1,15 @@
+// Add this to use jquery to test on the console 
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+
+// Gets all user information 
+$.getJSON('https://ipapi.co/json/', function(data){
+  let country = data.country;
+  let region = data.region;
+  if(country === "US"){
+    console.log("This user's country is " + country + " and state is " + region + ", so we should display DGPs.")
+  } else {
+    console.log("This user's country is not in the US, so we should display short courses.")
+  }
+}) 
